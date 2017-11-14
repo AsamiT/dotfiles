e94caed21a9970db6c567515f57d7af2ec02504d
Asami Tachibana <atachibana2612@gmail.com>
2017-11-14 10:20:54 -0500

    First commit

---
+++ macos/push.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+find ~/Git\ Repos/. -maxdepth 1 -type d \( ! -name . \) -print0 | xargs -0 -L1 sh -c 'cd "$0" && pwd && git add . && git commit -m "Added by auto-script" && git push'
+
