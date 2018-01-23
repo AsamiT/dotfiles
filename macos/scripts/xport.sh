99bacd4c867154624f4d6f10e4948dc6fd2ccf69
Asami Tachibana <atachibana2612@gmail.com>
2018-01-23 16:07:40 -0500

    Added by auto-script

--- macos/scripts/xport.sh
+++ macos/scripts/xport.sh
@@ -17,18 +17,18 @@ mkdir -p "$MASTER"/"$PDF"
 for FILE in "$@"
 do
     echo "Exporting $FILE to html."
-    pandoc -s "$FILE">> ./"$MASTER"/"$HTML"/"$FILE".html
+    pandoc -s "$FILE">> ./"$MASTER"/"$HTML"/"${FILE%%.*}".html
 done
 echo "All files exported to HTML."
 for FILE in "$@"
 do
     echo "Exporting $FILE into markdown..."
-    pandoc -s "$FILE" -t markdown -o ./"$MASTER"/"$MD"/"$FILE".md
+    pandoc -s "$FILE" -t markdown -o ./"$MASTER"/"$MD"/"${FILE%%.*}".md
 done
 echo "All files exported to markdown."
 for FILE in "$@"
 do
 	echo "Exporting $FILE into a PDF..."
-	pandoc -s "$FILE" -o ./"$MASTER"/"$PDF"/"$FILE".pdf
+	pandoc -s "$FILE" -o ./"$MASTER"/"$PDF"/"${FILE%%.*}".pdf
 done
 echo "All files exported to PDF."
