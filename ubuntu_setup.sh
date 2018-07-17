c65e3cbd715ed5adea64b13cba289b9caff6bfc6
J.M <31327410+AsamiT@users.noreply.github.com>
2018-07-17 18:02:43 -0400

    Update ubuntu_setup.sh

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -10,8 +10,8 @@ echo "First, we'll grab an essential utility, and update the repositories."
 ## Installer for ukuu, a kernel version utility
 echo "Installing ukuu..."
 apt-add-repository -y ppa:teejee2008/ppa
-apt update
-apt install ukuu
+apt -qq update
+apt -qq install ukuu
 ## Finish ukuu
 
 echo "Updating repository and upgrading packages..."
@@ -25,23 +25,23 @@ read -n 1 -s ## wait for keystroke to continue
 echo "Next, we're going to install some essential programs."
 if ! [ -x "$(command -v neofetch)" ]; then ##   Neofetch, CLI utility to display system info
     echo "neofetch isn't installed. Let's fix that."
-    apt install neofetch
+    apt -qq install neofetch
 fi
 if ! [ -x "$(command -v vlc)" ]; then ##    VLC, media player
     echo "VLC isn't installed. Let's fix that."
-    apt install vlc
+    apt -qq install vlc
 fi
 if ! [ -x "$(command -v git)" ]; then ##    Git, for repository management
     echo "Git isn't installed. Let's fix that."
-    apt install git
+    apt -qq install git
 fi
 if ! [ -x "$(command -v snap)" ]; then ##   Snap, Ubuntu's new package system.
     echo "You can't install snap packages right now. Let's fix that."
-    apt install snapd-xdg-open
+    apt -qq install snapd-xdg-open
 fi
 if ! [ -x "$(command -v discord)" ]; then ## Discord, chat program
     echo "Discord isn't installed. Let's fix that."
-    snap install discord
+    snap -qq install discord
 fi
 ##  Telegram
 ##  Atom
@@ -52,7 +52,7 @@ fi
 ##  Steam
 if ! [ -x "$(command -v cmus)" ]; then ## cmus, CLI music player
     echo "cmus isn't installed. Let's fix that."
-    snap install discord
+    apt -qq install cmus
 fi
 printf "All done!"
 exit 0
