5c3a550955c9d8812ff13c9e5620407060a08a9f
J.M <31327410+AsamiT@users.noreply.github.com>
2018-07-17 10:17:18 -0400

    Update and rename setup_distro.sh to ubuntu_setup.sh

---
+++ ubuntu_setup.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+if [[ $UID != 0 ]]; then
+    echo "Please run this script with sudo:"
+    echo "sudo $0 $*"
+    exit 1
+fi
+echo "This program will set up your Ubuntu-based Linux environment to your liking, Robin."
+if ! [ -x "$(command -v vlc)" ]; then
+    echo "VLC not installed. Installing..."
+    apt-get install vlc
+fi
+if ! [ -x "$(command -v git)" ]; then
+    echo "Git not installed. Installing..."
+    apt-get install git
+fi
+if ! [ -x "$(command -v snap)" ]; then
+    echo "You can't install snap packages right now. Let's fix that."
+    apt install snapd-xdg-open
+fi
+if ! [ -x "$(command -v discord)" ]; then
+    echo "Discord isn't installed. Let's fix that."
+    snap install discord
+fi
+printf "All done!"
+exit 0
