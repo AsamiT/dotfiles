9d5083b7fcfb41963828e9b7575ecd9a388c971d
J.M <31327410+AsamiT@users.noreply.github.com>
2018-07-17 10:21:41 -0400

    Update ubuntu_setup.sh
    I forgot that Canonical stopped using "-get" with apt. My age is showing.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -5,13 +5,16 @@ if [[ $UID != 0 ]]; then
     exit 1
 fi
 echo "This program will set up your Ubuntu-based Linux environment to your liking, Robin."
+if ! [ -x "$(command -v neofetch)" ]; then
+    echo "Neofetch isn't installed. Installing."
+    apt install neofetch
 if ! [ -x "$(command -v vlc)" ]; then
     echo "VLC not installed. Installing..."
-    apt-get install vlc
+    apt install vlc
 fi
 if ! [ -x "$(command -v git)" ]; then
     echo "Git not installed. Installing..."
-    apt-get install git
+    apt install git
 fi
 if ! [ -x "$(command -v snap)" ]; then
     echo "You can't install snap packages right now. Let's fix that."
