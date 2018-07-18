bba083d631dc87d482744718caad103f1efddd27
J.M <31327410+AsamiT@users.noreply.github.com>
2018-07-18 06:07:16 -0400

    Update ubuntu_setup.sh

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -25,6 +25,9 @@ printf "\n\nPress any key to continue.\n########################\n"
 read -n 1 -s ## wait for keystroke to continue
 
 echo "Next, we're going to install some essential programs."
+printf "\n\nPress any key to continue.\n"
+read -n 1 -s ## wait for keystroke to continue
+
 if ! [ -x "$(command -v neofetch)" ]; then ##   Neofetch, CLI utility to display system info
     echo "neofetch isn't installed. Let's fix that."
     apt-get -qq install neofetch
@@ -44,7 +47,7 @@ fi
 if ! [ -x "$(command -v telegram-desktop)"]; then ##	Telegram, messaging service
     echo "Telegram isn't installed. Let's fix that."
     apt-get -qq install telegram-desktop
-fi 
+fi
 if ! [ -x "$(command -v atom)"]; then ##	Atom, text editor
     echo "Atom isn't installed. Let's fix that."
     curl -sL https://packagecloud.io/AtomEditor/atom/gpgkey | sudo apt-key add -
@@ -68,9 +71,11 @@ if ! [ -x "$(command -v ffmpeg)"]; then ##  	ffmpeg
     echo "ffmpeg isn't installed. Let's fix that."
     apt-get -qq install ffmpeg youtube-dl
 fi
-##  Wine (TBD)
-##  DXVK (TBD)
-##  Steam (TBD)
+
+if ! [ -x "$(command -v steam)" ]; then ## Steam, gaming platform
+    echo "steam isn't installed. Let's fix that."
+    apt-get -qq install steam
+fi
 if ! [ -x "$(command -v cmus)" ]; then ## cmus, CLI music player
     echo "cmus isn't installed. Let's fix that."
     apt-get -qq install cmus
@@ -83,12 +88,40 @@ if ! [ -x "$(command -v zsh)" ]; then ##	install zsh, a replacement for bash
     echo "You don't have zsh. Let's fix that."
     apt-get -qq install zsh
 fi
+
+###############################################################
+#### These programs are a little more involved, ###############
+#### and require more effort.       ###########################
+###############################################################
+
+echo "Installing wine..."
+dpkg --add-architecture i386
+wget -nc https://dl.winehq.org/wine-builds/Release.key
+apt-key add Release.key
+apt-add-repository https://dl.winehq.org/wine-builds/ubuntu/
+apt-get -qq install --install-recommends winehq-stable
+clear
+
+echo "Installing vulkan, and updating GPU drivers..."
+apt install libvulkan1 libvulkan-dev vulkan-utils
+add-apt-repository ppa:graphics-drivers/ppa
+apt update && apt upgrade
+apt install vulkan
+
+printf "\nYou want to install Lutris and DXVK at a future time, all things considered.\nBut because it is not a necessity in this script, it has been excluded.\n\n"
+
 ###############################################################
 #### Doing some configuration work. ###########################
 ###############################################################
 
+echo "Downloading default wallpaper..."
+mkdir -p ~/Pictures/Wallpapers ##create default Wallpaper directory
+wget https://raw.githubusercontent.com/AsamiT/dotfiles/master/georgeVI.png -O ~/Pictures/Wallpapers/georgeVI.png ##retrieve default wallpaper
+gsettings set org.gnome.desktop.background picture-uri file:///home/$USER/Pictures/Wallpapers/georgeVI.png ##apply
+echo "done."
+
 if [ -x "$(command -v zsh)" ]; then ##	install oh-my-zsh!
-	echo "zsh" >> /etc/shells    
+	echo "zsh" >> /etc/shells
 	chsh -s $(which zsh)
 	curl -fsSL 'https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh'
 	## we're going to add a portion that clones and copies zshrc from my dotfiles git repo to the ~ directory.
